bafb943598d8cb59c388d2019361355ee05b9224
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-04-16 16:08:12 +0300

    16:00

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const path = require('path');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const session = require('express-session');
 
 const profileRoutes = require('./routes/profile');
-
 const homeRoutes = require('./routes/home');
 const authRoutes = require('./routes/auth');
 const reqRoutes = require('./routes/req');
+const varMiddleware = require('./middleware/variables')
 
-const MONGODB_URI = 'mongodb+srv://slava:9W3f7yGwNWj1TmPN@cluster0-qtwws.mongodb.net/test?retryWrites=true&w=majority';
+// const MONGODB_URI = 'mongodb+srv://slava:9W3f7yGwNWj1TmPN@cluster0-qtwws.mongodb.net/test?retryWrites=true&w=majority';
 
 
 const app = express();
@@ -29,19 +30,26 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
+app.use(session({
+  secret: 'secret secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(varMiddleware);
 app.use(morgan('dev'));
 
 
 app.use('/', homeRoutes);
 app.use('/auth', authRoutes);
 app.use('/requests', reqRoutes);
+app.use('/profile', profileRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true
--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,34 @@ router.get('/login', async (req, res) => {
   })
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const {
+      email,
+      password
+    } = req.body;
+    const candidate = await User.findOne({
+      email
+    });
+    if (candidate) {
+      req.session.user = candidate;
+      req.session.isAuthenticated = true;
+      req.session.save(err => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('complete<<<<<<<<<<<<<<<<<<<<')
+          return res.redirect('/')
+        }
+      })
+    } else {
+      return res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 router.post('/register', async (req, res) => {
   try {
     const {
@@ -40,6 +68,7 @@ router.post('/register', async (req, res) => {
     console.log(e);
   }
 });
+
 
 
 module.exports = router
