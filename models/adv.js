ea1bd7d201be09e266920cbda1cc8465c0e166bb
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-04-16 19:49:31 +0300

    merge conflict

--- models/adv.js
+++ models/adv.js
@@ -7,7 +7,6 @@ const {
 const advSchema = new Schema({
   title: {
     type: String,
-<<<<<<< HEAD
     required: true,
   },
   content: {
@@ -25,31 +24,8 @@ const advSchema = new Schema({
   bid: {
     type: Schema.Types.ObjectId,
     ref: 'Bid',
-=======
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  photo: {
-    type: String,
-    required: true
-  },
-  person: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  bid: {
-    type: Schema.Types.ObjectId,
-    ref: 'Adv'
->>>>>>> master
   },
 });
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 module.exports = model('Adv', advSchema);
