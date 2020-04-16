b4f8d424510f76b166595a6f808ef28c535b445a
Samolevich <samolevich@gmail.com>
2020-04-16 14:11:44 +0300

    making routes

--- routes/req.js
+++ routes/req.js
@@ -1,13 +1,24 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+// coinst Resp = require('../models/resp')
 
-const requests = [1, 2, 3]
+const requests = [{}, {}, {}, {}, {}, {}]
 
 router.get('/', (req, res) => {
+  // requests = await Resp.mostRecent();
   res.render('moderate/requests', {
     title: 'Заявки',
     isReq: true,
     requests
   });
+});
+
+router.delete('/:id', async (req, res) => {
+  // await Resp.deleteOne({ id: req.params.id})
+});
+
+router.get('/:id/edit', async (req, res) => {
+  // let entry = await Resp.findById({ id: req.params.id})
 });
 
 module.exports = router;
