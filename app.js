5da6af2740926100c31e06679903456f0c5513aa
Olesya <ananeva.olesya@yandex.ru>
2020-04-16 03:06:40 +0300

    profile page not ideal

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const morgan = require('morgan');
 const homeRoutes = require('./routes/home');
 const authRoutes = require('./routes/auth');
-
+const profileRoutes = require('./routes/profile');
 
 
 const app = express();
@@ -13,19 +14,20 @@ app.set('view options', {
   layout: 'layouts/main'
 });
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
-}))
+  extended: true,
+}));
+app.use(morgan('dev'));
 
 app.use('/', homeRoutes);
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 
-
-app.listen(3000)
+app.listen(3000, () => console.log('server has been started'));
 module.exports = app;
