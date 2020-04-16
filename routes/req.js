d29418a0bf734406a843d2fd71f04da02b0c5c5b
Samolevich <samolevich@gmail.com>
2020-04-16 14:44:48 +0300

    add accep/decline buttons

--- routes/req.js
+++ routes/req.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 // coinst Resp = require('../models/resp')
 
-const requests = [{}, {}, {}, {}, {}, {}]
+const requests = [{id: 1}, {id: 2}, {id: 3}, {}, {}, {}]
 
 router.get('/', (req, res) => {
   // requests = await Resp.mostRecent();
@@ -13,12 +13,20 @@ router.get('/', (req, res) => {
   });
 });
 
-router.delete('/:id', async (req, res) => {
-  // await Resp.deleteOne({ id: req.params.id})
+router.get('/:id/:status', async (req, res) => {
+  if (!['accept', 'decline'].includes(req.params.status)) {
+    res.end('Invalid');
+    return;
+  }
+  // let doc = await Resp.findById({ id: req.params.id} )
+  // doc..status = req.params.status;
+  requests[0].status = req.params.status;
+  doc.save();
+  res.send('ok')
 });
 
-router.get('/:id/edit', async (req, res) => {
-  // let entry = await Resp.findById({ id: req.params.id})
-});
+// router.get('/:id/decline', async (req, res) => {
+//   // let entry = await Resp.findById({ id: req.params.id})
+// });
 
 module.exports = router;
